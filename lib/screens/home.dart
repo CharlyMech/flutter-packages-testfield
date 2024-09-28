f7deefce7d5e51a3a7e248ce82d6bf08d7b5ae7b
CharlyMech <sanchezreciocarlos99@gmail.com>
2024-09-28 22:01:37 +0200

    feat: start implementing home content

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_kitchen/components/bottom_sheet_content.dart';
+import 'package:flutter_kitchen/components/custom_card.dart';
+import 'package:flutter_kitchen/constants/packages.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
+
+  List<Widget> _buildHomeListContent() {
+    List<Widget> homeListContent = packages
+        .map((pkg) => CustomCard(
+            width: double.infinity,
+            padding: const EdgeInsets.all(15),
+            margin: const EdgeInsets.symmetric(vertical: 5),
+            borderRadius: const BorderRadius.all(Radius.circular(10)),
+            child: Text(pkg.name)))
+        .toList();
+    return homeListContent;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,12 +27,30 @@ class Home extends StatelessWidget {
         elevation: 5,
         shadowColor: Theme.of(context).shadowColor,
       ),
-      body: Center(child: Text('home')),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView.builder(
+          itemCount: packages.length,
+          itemBuilder: (context, index) {
+            final pkg = packages[index];
+            return CustomCard(
+              width: double.infinity,
+              padding: const EdgeInsets.all(15),
+              margin: const EdgeInsets.symmetric(vertical: 5),
+              borderRadius: const BorderRadius.all(Radius.circular(10)),
+              child: Text(pkg.name),
+            );
+          },
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showBarModalBottomSheet<void>(
-            // expand: true,
             context: context,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10), topRight: Radius.circular(10)),
+            ),
             builder: (BuildContext context) => const BottomSheetContent(),
           );
         },
