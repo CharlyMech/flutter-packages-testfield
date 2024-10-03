0899f130405a8ae5059bad08c603d503d3eb40c2
CharlyMech <sanchezreciocarlos99@gmail.com>
2024-10-03 07:49:00 +0200

    wip

---
+++ lib/screens/packages/fl_chart/fl_chart_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FlChartScreen extends StatelessWidget {
+  const FlChartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('FL Charts'),
+    );
+  }
+}
---
+++ lib/screens/packages/rive/rive_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RiveScreen extends StatelessWidget {
+  const RiveScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Rive'),
+    );
+  }
+}
