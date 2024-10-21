b07b8723ba4afcb078d6d2f6862c8b2063f46162
charlymech <sanchezreciocarlos99@gmail.com>
2024-10-21 12:56:35 +0200

    feat: add animation on screen change

--- lib/config/router.dart
+++ lib/config/router.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_kitchen/layouts/package_layout.dart';
 import 'package:flutter_kitchen/screens/home.dart';
 import 'package:flutter_kitchen/screens/packages/fl_chart/fl_chart_screen.dart';
@@ -15,24 +16,70 @@ final goRouter = GoRouter(routes: [
   ),
   GoRoute(
     path: '/rive',
-    builder: (context, state) => const PackageLayout(
-      title: 'Rive',
-      child: RiveScreen(),
-    ),
+    pageBuilder: (context, state) => CustomTransitionPage(
+        key: state.pageKey,
+        child: const PackageLayout(
+          title: 'Rive',
+          child: RiveScreen(),
+        ),
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          return SlideTransition(
+            position: Tween<Offset>(
+              begin: const Offset(1, 0),
+              end: Offset.zero,
+            ).animate(animation),
+            child: child,
+          );
+        },
+        transitionDuration: const Duration(milliseconds: 300)),
   ),
   GoRoute(
     path: '/fl_chart',
-    builder: (context, state) =>
-        const PackageLayout(title: 'FL Chart', child: FlChartScreen()),
+    pageBuilder: (context, state) => CustomTransitionPage(
+        key: state.pageKey,
+        child: const PackageLayout(title: 'FL Chart', child: FlChartScreen()),
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          return SlideTransition(
+            position: Tween<Offset>(
+              begin: const Offset(1, 0),
+              end: Offset.zero,
+            ).animate(animation),
+            child: child,
+          );
+        },
+        transitionDuration: const Duration(milliseconds: 300)),
   ),
   GoRoute(
     path: '/logger',
-    builder: (context, state) =>
-        const PackageLayout(title: 'FL Chart', child: LoggerScreen()),
+    pageBuilder: (context, state) => CustomTransitionPage(
+        key: state.pageKey,
+        child: const PackageLayout(title: 'FL Chart', child: LoggerScreen()),
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          return SlideTransition(
+            position: Tween<Offset>(
+              begin: const Offset(1, 0),
+              end: Offset.zero,
+            ).animate(animation),
+            child: child,
+          );
+        },
+        transitionDuration: const Duration(milliseconds: 300)),
   ),
   GoRoute(
     path: '/introduction_screen',
-    builder: (context, state) => const PackageLayout(
-        title: 'FL Chart', child: IntroductionScreenScreen()),
+    pageBuilder: (context, state) => CustomTransitionPage(
+        key: state.pageKey,
+        child: const PackageLayout(
+            title: 'FL Chart', child: IntroductionScreenScreen()),
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          return SlideTransition(
+            position: Tween<Offset>(
+              begin: const Offset(1, 0),
+              end: Offset.zero,
+            ).animate(animation),
+            child: child,
+          );
+        },
+        transitionDuration: const Duration(milliseconds: 300)),
   ),
 ]);
