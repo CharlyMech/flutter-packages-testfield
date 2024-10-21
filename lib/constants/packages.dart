974021fedcfe93b4b3a92598b34326318a2facbf
charlymech <sanchezreciocarlos99@gmail.com>
2024-10-21 07:59:23 +0200

    feat: update package descriptions, make them shorter

--- lib/constants/packages.dart
+++ lib/constants/packages.dart
@@ -6,7 +6,7 @@ final List<Package> packages = [
       id: UniqueKey().hashCode,
       name: 'Rive',
       shortDescr:
-          'Rive lets you create rich animations with powerful tools, making them interactive and ready to use across platforms.',
+          'Create rich animations with powerful tools, making them interactive and ready to use across platforms.',
       documentationLink: 'https://rive.app/',
       pubDevLink: 'https://pub.dev/packages/rive',
       route: '/rive'),
@@ -14,21 +14,21 @@ final List<Package> packages = [
       id: UniqueKey().hashCode,
       name: 'Logger',
       shortDescr:
-          'Logger offers a flexible logging library for your Flutter app, allowing you to easily log messages, errors, and debug information at different levels.',
+          'Flexible logging library for your Flutter app, allowing you to easily log at different levels.',
       pubDevLink: 'https://pub.dev/packages/logger',
       route: '/logger'),
   Package(
       id: UniqueKey().hashCode,
       name: 'FL Chart',
       shortDescr:
-          'FL Chart provides a rich set of customizable charts like line charts, bar charts, pie charts, and more to visualize your data effectively in your Flutter app.',
+          'Rich set of customizable charts to visualize your data effectively in your Flutter app.',
       pubDevLink: 'https://pub.dev/packages/fl_chart',
       route: '/fl_chart'),
   Package(
       id: UniqueKey().hashCode,
       name: 'Introduction Screen',
       shortDescr:
-          'Introduction Screen helps you create informative and visually appealing onboarding screens to guide new users through your Flutter app\'s features.',
+          'Create informative and visually appealing onboarding screens to guide new users through app\'s features.',
       pubDevLink: 'https://pub.dev/packages/introduction_screen',
       route: '/introduction_screen'),
 ];
